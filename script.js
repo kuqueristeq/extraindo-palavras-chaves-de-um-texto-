a09148a8054517b8046cf8f15b4897db24dd0158
kuqueristeq <kulenetz.mateus@escola.pr.gov.br>
2025-09-19 09:10:06 -0300

    okk

--- script.js
+++ script.js
@@ -13,5 +13,15 @@ campoResultado.textContent = palavrasChave.join(", ");
 }
 function processaTexto(texto) {
 let palavras = texto.split(/\P{L}+/u);
+let frequencias ={};
+for (let i of palavras) {
+    frequencias[i] = 0;
+    for (let j of palavras) {
+        if (i==i) {
+            frequencias[i]++;
+        }
+    }
+}
+console.log(frequencias);
 return palavras;
 }
